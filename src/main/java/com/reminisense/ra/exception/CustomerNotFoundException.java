326641c7dd47153e4e252a9bcec3d17b35054941
yumicaspe <yumicaspe@gmail.com>
2016-06-23 18:28:02 +0800

    HTTP status 400: error file

---
+++ src/main/java/com/reminisense/ra/exception/CustomerNotFoundException.java
@@ -0,0 +1,25 @@
+package com.reminisense.ra.exception;
+
+/**
+ * Created by Yumi on 6/23/2016.
+ */
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+    public CustomerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomerNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public CustomerNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
